6859978d96d2b938a7e14d162c77657ce7161038
Nikola Lucic <nikola1983@yahoo.com>
2023-05-08 16:58:38 -0400

    added schemas folder with typeDefs and resolvers

--- Develop/server/models/Book.js
+++ Develop/server/models/Book.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose');
+const { Schema } = require("mongoose");
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
@@ -22,7 +22,7 @@ const bookSchema = new Schema({
   link: {
     type: String,
   },
-  title: {
+  bookTitle: {
     type: String,
     required: true,
   },
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    books: [Book]!
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    bookTitle: String
+    description: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    books(username: String): [Book]
+    book(bookId: ID!): Book
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBook(bookTitle: String!): Book
+    removeBook(bookId: ID!): Book
+  }
+`;
+
+module.exports = typeDefs;
